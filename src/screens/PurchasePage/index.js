a0ee1a55ecc9562e7362d3b2da066eb1790380f5
lucacicco <lucasciccomy@gmail.com>
2020-08-26 13:01:24 -0300

    tiny changes on purchasepage

--- src/screens/PurchasePage/index.js
+++ src/screens/PurchasePage/index.js
@@ -1,30 +1,32 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { Animated, Keyboard, Platform, StyleSheet } from 'react-native';
+import { Keyboard, Platform, Image} from 'react-native';
 import { CreditCardInput } from "react-native-credit-card-input";
 
 import { 
     Container, 
-    SubmitButton
+    SubmitButton,
+    SecurityView,
+    SecurityText,
+    PaymentView
 } from './styles';
 
 function PurchasePage(){
-
-    const typeOfPlatform = Platform.OS === 'ios'
-    const typeOfkeyboardType = typeOfPlatform ? 'numbers-and-punctuation' : 'numeric'
-
     const [CardNumber, setCardNumber] = useState('')
     const [CVC, setCVC] = useState('')
     const [ExpireDate, setExpireDate] = useState('')
     const [Owner, setOwner] = useState('')  
     const [typeCard, setTypeCard] = useState('')
+    const [Valid, setValid] = useState(false)
 
-    const setCardValues = ({ values }) => {
+    const setCardValues = ({ values, valid }) => {
         const { number, cvc, type, name, expiry } = values
+
         setCardNumber(number)
         setCVC(cvc)
         setExpireDate(expiry)
         setOwner(name)
         setTypeCard(type)
+        setValid(valid)
     }
 
     const handleSubmit = () => {
@@ -36,43 +38,55 @@ function PurchasePage(){
     
     return(
         <Container>
-            <CreditCardInput
-                autoFocus
-                requiresName={true}
-                requiresCVC={true}
-                validColor="black"
-                invalidColor="red"
-                placeholderColor="darkgray"
-                labels={{
-                    number: 'NÚMERO DO CARTÃO',
-                    expiry: 'VALIDADE',
-                    cvc: "CVC/CCV",
-                    name: 'NOME COMPLETO'
-                }}
-                labelStyle={{
-                    color: 'black',
-                    fontSize: 15
-                }}
-                inputStyle={{
-                    color: 'black',
-                    fontSize: 19
-                }}
-                placeholders={{
-                    name: "Nome completo",
-                    number: "1234 5678 1234 5678",
-                    expiry: "MM/YY",
-                    cvc: "CVC",
-                }}
-                allowScroll={true}
-                onChange={setCardValues}   
+            <PaymentView>
+                <CreditCardInput
+                    autoFocus
+                    requiresName={true}
+                    requiresCVC={true}
+                    validColor="black"
+                    invalidColor="red"
+                    placeholderColor="darkgray"
+                    labels={{
+                        number: 'NÚMERO DO CARTÃO',
+                        expiry: 'VALIDADE',
+                        cvc: "CVC/CCV",
+                        name: 'NOME COMPLETO'
+                    }}
+                    labelStyle={{
+                        color: 'black',
+                        fontSize: 15
+                    }}
+                    inputStyle={{
+                        color: 'black',
+                        fontSize: 19
+                    }}
+                    placeholders={{
+                        name: "Nome completo",
+                        number: "1234 5678 1234 5678",
+                        expiry: "MM/YY",
+                        cvc: "CVC",
+                    }}
+                    allowScroll={true}
+                    onChange={setCardValues}   
                 />
+
                 <SubmitButton onPress={handleSubmit}>
                     Realizar pagamento
                 </SubmitButton>
+            </PaymentView>
+
+            <SecurityView>
+                <Image 
+                    source={require('../../assets/security_icon.png')}
+                    style={{ height: 50, margin: 5}}
+                    resizeMode="contain"
+                />
+                <SecurityText>
+                    Seus dados estão protegidos
+                </SecurityText>
+            </SecurityView>
         </Container>
     )
 }
-
-const styles = StyleSheet.create({}) 
 
 export default PurchasePage
